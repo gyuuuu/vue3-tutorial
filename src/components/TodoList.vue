a516e336bbfbbdaa417d6230781a4d38ff0ac6bd
gyuuuu <kai9702@gmail.com>
2021-06-24 01:17:42 +0900

    delete todo

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -35,8 +35,13 @@ export default {
       context.emit('toggle-todo', index);
     };
 
+    const deleteTodo = (index) => {
+      context.emit('delete-todo', index);
+    };
+
     return {
       toggleTodo,
+      deleteTodo,
     };
   },
 };
