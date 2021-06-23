fb857e84ddd3ae33d9af5490bee0a1d8090cac31
gyuuuu <kai9702@gmail.com>
2021-06-23 23:01:50 +0900

    이벤트 & 변경적용 x

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
 <template>
   <div class="name">
-    {{ greeting(name) }}
+    {{ name }}
   </div>
+  <button 
+    class="btn btn-primary"
+    v-on:click="updateName"
+  >
+  Click
+  </button>
 </template>
 
 <script>
 export default {
   setup(){
-    const name = "Gyu";
+    let name = "Gyu";
 
-    const greeting = (name) => {
-      return 'Hello, ' + name;
-    };
+    // const greeting = (name) => {
+    //   return 'Hello, ' + name;
+    // };
+
+    const updateName = () => {
+      name = 'Gyuuuu';
+      console.log(name);
+    }
 
     return {
       name,
-      greeting
+      updateName
     };
   }
 }
