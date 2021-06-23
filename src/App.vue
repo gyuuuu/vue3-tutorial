844e4994158ea4f6be7a87f250f6567a463685bf
gyuuuu <kai9702@gmail.com>
2021-06-24 00:48:25 +0900

    delete item

--- src/App.vue
+++ src/App.vue
@@ -12,22 +12,35 @@
           />
         </div>
         <div>
-          <button class="btn btn-primary" type="submit">Add</button>
+          <button class="btn btn-primary" type="submit">
+            Add
+          </button>
         </div>
       </div>
-      <div v-show="hasError" style="color: red">This field cannot be empty</div>
+      <div v-show="hasError" style="color: red">
+        This field cannot be empty
+      </div>
     </form>
-    <div v-for="todo in todos" :key="todo.id" class="card mt-2">
-      <div class="card-body p-2"></div>
-      <div class="form-check">
-        <input
-          class="form-check-input"
-          type="checkbox"
-          v-model="todo.completed"
-        />
-        <label class="form-check-label" :class="{ todo: todo.completed }">{{
-          todo.subject
-        }}</label>
+    <div v-if="!todos.length">
+      추가된 Todo가 없습니다
+    </div>
+    <div v-for="(todo, index) in todos" :key="todo.id" class="card mt-2">
+      <div class="card-body p-2 d-flex align-items-center">
+        <div class="form-check flex-grow-1">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            v-model="todo.completed"
+          />
+          <label class="form-check-label" :class="{ todo: todo.completed }">
+            {{ todo.subject }}
+          </label>
+        </div>
+        <div>
+          <button class="btn btn-danger btn-sm" @click="deleteTodo(index)">
+            Delete
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -35,16 +48,11 @@
 
 <script>
 import { ref } from 'vue';
-
 export default {
   setup() {
     const todo = ref('');
     const todos = ref([]);
     const hasError = ref(false);
-    const todoStyle = {
-      textDecoration: 'line-through',
-      color: 'gray',
-    };
 
     const onSubmit = () => {
       if (todo.value === '') {
@@ -60,12 +68,16 @@ export default {
       }
     };
 
+    const deleteTodo = (index) => {
+      todos.value.splice(index, 1);
+    };
+
     return {
       todo,
       todos,
       hasError,
-      todoStyle,
       onSubmit,
+      deleteTodo,
     };
   },
 };
