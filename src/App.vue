1e4fbdd8957de14d46b04ee6c0c89131dcb790ea
gyuuuu <kai9702@gmail.com>
2021-06-24 01:01:40 +0900

    emit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <h2>To-Do List</h2>
-    <TodoSimpleForm />
+    <TodoSimpleForm @add-todo="addTodo" />
     <div v-if="!todos.length">
       추가된 Todo가 없습니다
     </div>
@@ -37,22 +37,10 @@ export default {
   },
 
   setup() {
-    const todo = ref('');
     const todos = ref([]);
-    const hasError = ref(false);
 
-    const onSubmit = () => {
-      if (todo.value === '') {
-        hasError.value = true;
-      } else {
-        todos.value.push({
-          id: Date.now(),
-          subject: todo.value,
-          completed: false,
-        });
-        hasError.value = false;
-        todo.value = '';
-      }
+    const addTodo = (todo) => {
+      todos.value.push(todo);
     };
 
     const deleteTodo = (index) => {
@@ -60,10 +48,8 @@ export default {
     };
 
     return {
-      todo,
       todos,
-      hasError,
-      onSubmit,
+      addTodo,
       deleteTodo,
     };
   },
