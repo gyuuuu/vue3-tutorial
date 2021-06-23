984aa55f5921566ae5b14598852365708eee2a38
gyuuuu <kai9702@gmail.com>
2021-06-23 22:50:44 +0900

    vue component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>gyu</div>
+  <div class="name">{{ name }}</div>
 </template>
+
+<script>
+export default {
+  setup(){
+    const name = "Gyu";
+
+    return {
+      name
+    };
+  }
+}
+</script>
+
+<style>
+  .name {
+    color: blue;
+  }
+</style>
