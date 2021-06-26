25107e878aa18c8b39553473a6188be0202ca4ee
gyuuuu <kai9702@gmail.com>
2021-06-26 17:21:43 +0900

    add todo to db

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
     />
     <hr />
     <TodoSimpleForm @add-todo="addTodo" />
+    <div style="color: red">{{ error }}</div>
 
     <div v-if="!filteredTodos.length">
       There si noting to display
@@ -25,6 +26,7 @@
 import { ref, computed } from 'vue';
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
 import TodoList from './components/TodoList.vue';
+import axios from 'axios';
 
 export default {
   components: {
@@ -34,6 +36,7 @@ export default {
 
   setup() {
     const todos = ref([]);
+    const error = ref('');
     const searchText = ref('');
     const filteredTodos = computed(() => {
       if (searchText.value) {
@@ -45,7 +48,20 @@ export default {
     });
 
     const addTodo = (todo) => {
-      todos.value.push(todo);
+      error.value = '';
+      axios
+        .post('http://localhost:3000/todos', {
+          subject: todo.subject,
+          completed: todo.completed,
+        })
+        .then((res) => {
+          console.log(res);
+          todos.value.push(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+          error.value = 'Something went wrong.';
+        });
     };
 
     const toggleTodo = (index) => {
@@ -60,6 +76,7 @@ export default {
       todos,
       searchText,
       filteredTodos,
+      error,
       addTodo,
       toggleTodo,
       deleteTodo,
