26f4f1b5c6f8946c4685ac5ef48c8289e6d4735d
gyuuuu <kai9702@gmail.com>
2021-06-26 22:58:00 +0900

    bug fix

--- src/App.vue
+++ src/App.vue
@@ -98,7 +98,7 @@ export default {
       currentPage.value = page;
       try {
         const res = await axios.get(
-          `http://localhost:3000/todos?subject_like=${searchText.value}&_page=${page}&_limit=${limit}`
+          `http://localhost:3000/todos?_sort=id&_order=desc&subject_like=${searchText.value}&_page=${page}&_limit=${limit}`
         );
         numberOfTodos.value = res.headers['x-total-count'];
         todos.value = res.data;
@@ -113,11 +113,11 @@ export default {
     const addTodo = async (todo) => {
       error.value = '';
       try {
-        const res = await axios.post('http://localhost:3000/todos', {
+        await axios.post('http://localhost:3000/todos', {
           subject: todo.subject,
           completed: todo.completed,
         });
-        todos.value.push(res.data);
+        getTodos(1);
       } catch (error) {
         console.log(error);
         error.value = 'Something went wrong.';
@@ -143,7 +143,7 @@ export default {
       const id = todos.value[index].id;
       try {
         await axios.delete('http://localhost:3000/todos/' + id);
-        todos.value.splice(index, 1);
+        getTodos(1);
       } catch (error) {
         console.log(error);
         error.value = 'Something went wrong.';
