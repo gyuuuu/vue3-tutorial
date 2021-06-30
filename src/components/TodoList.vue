acfdd9cde7aebafda3e165c46cd982e97c381078
gyuuuu <kai9702@gmail.com>
2021-06-30 23:44:43 +0900

    modal close event

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -18,18 +18,24 @@
         </span>
       </div>
       <div>
-        <button class="btn btn-danger btn-sm" @click.stop="deleteTodo(index)">
+        <button class="btn btn-danger btn-sm" @click.stop="openModal(todo.id)">
           Delete
         </button>
       </div>
     </div>
   </div>
+  <Modal v-if="showModal" @close="closeModal" />
 </template>
 
 <script>
 import { useRouter } from 'vue-router';
+import Modal from '@/components/Modal.vue';
+import { ref } from 'vue';
 
 export default {
+  components: {
+    Modal,
+  },
   props: {
     todos: {
       type: Array,
@@ -40,8 +46,19 @@ export default {
 
   setup(props, { emit }) {
     const router = useRouter();
+    const showModal = ref(false);
+    const todoDeleteId = ref(null);
     const toggleTodo = (index, event) => {
       emit('toggle-todo', index, event.target.checked);
+    };
+
+    const openModal = (id) => {
+      todoDeleteId.value = id;
+      showModal.value = true;
+    };
+    const closeModal = () => {
+      todoDeleteId.value = null;
+      showModal.value = false;
     };
 
     const deleteTodo = (index) => {
@@ -60,6 +77,9 @@ export default {
     };
 
     return {
+      showModal,
+      openModal,
+      closeModal,
       toggleTodo,
       deleteTodo,
       moveToPage,
